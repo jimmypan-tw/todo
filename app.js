59e2bff514db680eaa9ed183b0774c0cbeead3d0
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-06-21 10:19:02 +0800

    Add detail page

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.get('/todos/new', (req, res) => {
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/todos/:id', (req, res) => {
-    res.send('顯示 Todo 的詳細內容')
+    Todo.findById(req.params.id, (err, todo) => {
+        if (err) return console.log(err)
+        return res.render('detail', { todo: todo })
+    })
 })
 
 // 新增一筆  Todo
