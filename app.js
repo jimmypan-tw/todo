c5ac3a7b288cbbace754c84e49c5dbb89690bc52
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-06-24 22:18:17 +0800

    Add todos and home routers

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const methodOverride = require('method-override')
 // 設定 method-override
 app.use(methodOverride('_method'))
 
+// 引用並設定mongoose
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/todo', { useNewUrlParser: true })
 const db = mongoose.connection
@@ -30,86 +31,12 @@ db.once('open', () => {
     console.log('mongodb connected.')
 })
 
+// 載入todo model
 const Todo = require('./models/todo')
 
-// 設定路由
-// Todo 首頁
-app.get('/', (req, res) => {
-    Todo.find({})
-        .sort({ name: 'asc' })
-        .exec((err, todos) => {
-            // 把Todo model所有的資料都抓回來
-            if (err) return console.error(err)
-            return res.render('index', { todos: todos })
-        })
-})
-
-// 列出全部 Todo
-app.get('/todos', (req, res) => {
-    res.send('列出所有 Todo')
-})
-
-// 新增一筆 Todo 頁面
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-})
-
-// 顯示一筆 Todo 的詳細內容
-app.get('/todos/:id', (req, res) => {
-    Todo.findById(req.params.id, (err, todo) => {
-        if (err) return console.error(err)
-        return res.render('detail', { todo: todo })
-    })
-})
-
-// 新增一筆  Todo
-app.post('/todos', (req, res) => {
-    const todo = new Todo({
-        name: req.body.name
-    })
-
-    todo.save(err => {
-        if (err) return console.error(err)
-        return res.redirect('/')
-    })
-})
-
-// 修改 Todo 頁面
-app.get('/todos/:id/edit', (req, res) => {
-    Todo.findById(req.params.id, (err, todo) => {
-        if (err) return console.error(err)
-        return res.render('edit', { todo: todo })
-    })
-})
-
-// 修改 Todo
-app.put('/todos/:id', (req, res) => {
-    Todo.findById(req.params.id, (err, todo) => {
-        if (err) return console.error(err)
-        todo.name = req.body.name
-        if (req.body.done === 'on') {
-            todo.done = true
-        } else {
-            todo.done = false
-        }
-        todo.save(err => {
-            if (err) return console.error(err)
-            return res.redirect(`/todos/${req.params.id}`)
-        })
-    })
-})
-
-// 刪除 Todo
-app.delete('/todos/:id/delete', (req, res) => {
-    Todo.findById(req.params.id, (err, todo) => {
-        if (err) return console.error(err)
-        todo.remove(err => {
-            if (err) return console.error(err)
-            return res.redirect('/')
-        })
-    })
-})
-
+// 載入router
+app.use('/', require('./routes/home'))
+app.use('/todos', require('./routes/todo'))
 
 app.listen(port, () => {
     console.log('App is running...')
